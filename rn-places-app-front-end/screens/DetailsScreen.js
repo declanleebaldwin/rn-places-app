d6893401539bccd56ad69952e4cce915606f4110
declan baldwin <dbaldwin4@boxfusionconsulting.com>
2020-06-22 17:27:58 +0100

    adding map to details page

--- rn-places-app-front-end/screens/DetailsScreen.js
+++ rn-places-app-front-end/screens/DetailsScreen.js
@@ -1,9 +1,15 @@
-import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import MapView, { Marker } from "react-native-maps";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 
 function DetailsScreen({ route, navigation }) {
   const { place } = route.params;
-  console.log(place);
+  const [region, setRegion] = useState({
+    latitude: place.geometry.location.lat,
+    longitude: place.geometry.location.lng,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+  });
   return (
     <View>
       <View style={styles.headerContainer}>
@@ -11,6 +17,15 @@ function DetailsScreen({ route, navigation }) {
         <View>
           <Text style={styles.vicinity}>{place.vicinity}</Text>
         </View>
+        <MapView
+          style={styles.mapStyle}
+          region={region}
+          onRegionChangeComplete={(region) => setRegion(region)}
+        >
+          <Marker
+            coordinate={{ latitude: place.geometry.location.lat, longitude: place.geometry.location.lng }}
+          />
+        </MapView>
       </View>
     </View>
   );
@@ -18,15 +33,19 @@ function DetailsScreen({ route, navigation }) {
 
 const styles = StyleSheet.create({
   headerContainer: {
-    alignItems: 'center',
-    marginVertical: 20
+    alignItems: "center",
+    marginVertical: 20,
   },
   header: {
-    fontSize: 20
+    fontSize: 20,
   },
   vicinity: {
-    marginTop: 5
-  }
-})
+    marginTop: 5,
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
 
 export default DetailsScreen;
